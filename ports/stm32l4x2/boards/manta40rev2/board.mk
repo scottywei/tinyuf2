f256b73b83c472f5a1cf458cc579718bfed72710
Scott Wei <scot.wei@gmail.com>
2026-03-23 01:20:04 +0800

    Move stm32l4x2 shared clock to port root

--- ports/stm32l4x2/boards/manta40rev2/board.mk
+++ ports/stm32l4x2/boards/manta40rev2/board.mk
@@ -5,7 +5,7 @@ SRC_S += \
   $(ST_CMSIS)/Source/Templates/gcc/startup_stm32l412xx.s
 
 SRC_C += \
-  $(PORT_DIR)/boards/common/clock_stm32l412_hsi48.c
+  $(PORT_DIR)/board_clock.c
 
 # For flash-jlink target
 JLINK_DEVICE = stm32l412cb
